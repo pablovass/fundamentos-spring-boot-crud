1c2f2292aea2f2efea6dc4ed8235b7341db04d78
Pablo Vallejos <faktory.fm@live.com>
2023-12-07 22:59:46 -0300

    feat: query metod

--- store/src/main/java/com/pablovass/persistence/entity/ProductoClassRepository.java
+++ store/src/main/java/com/pablovass/persistence/entity/ProductoClassRepository.java
@@ -3,11 +3,20 @@ package com.pablovass.persistence.entity;
 import com.pablovass.persistence.repository.ProductoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoClassRepository {
     private ProductoRepository productoRepository;
 
-    public List<Producto>getAll(){
+    public List<Producto> getAll() {
         return (List<Producto>) productoRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
--- store/src/main/java/com/pablovass/persistence/repository/ProductoRepository.java
+++ store/src/main/java/com/pablovass/persistence/repository/ProductoRepository.java
@@ -1,7 +1,17 @@
 package com.pablovass.persistence.repository;
 
 import com.pablovass.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoRepository extends CrudRepository<Producto,Integer> {
+    @Query(value = "SELECT * FROM productos WHERE id_categoria= ? ", nativeQuery = true) //query nativa
+    List<Producto>findByIdCategoria(int idCategoria); //query method
+
+    List<Producto>findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock,boolean estado);
 }
