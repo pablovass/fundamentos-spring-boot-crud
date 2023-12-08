6879e5076686651302b0a4a2e89fce7e02b04502
Pablo Vallejos <faktory.fm@live.com>
2023-12-07 23:06:14 -0300

    feat:Implementar la anotación @Repository

--- store/src/main/java/com/pablovass/persistence/entity/ProductoClassRepository.java
+++ store/src/main/java/com/pablovass/persistence/entity/ProductoClassRepository.java
@@ -1,10 +1,11 @@
 package com.pablovass.persistence.entity;
 
 import com.pablovass.persistence.repository.ProductoRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoClassRepository {
     private ProductoRepository productoRepository;
 
@@ -18,5 +19,14 @@ public class ProductoClassRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad) {
         return productoRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+    public Optional<Producto>getProducto(int idProducto){
+        return productoRepository.findById(idProducto);
+    }
+    public Producto save(Producto producto){
+        return productoRepository.save(producto);
+    }
+    public void delete(int idProducto){
+        productoRepository.deleteById(idProducto);
     }
 }
--- store/src/main/java/com/pablovass/persistence/repository/ProductoRepository.java
+++ store/src/main/java/com/pablovass/persistence/repository/ProductoRepository.java
@@ -3,10 +3,11 @@ package com.pablovass.persistence.repository;
 import com.pablovass.persistence.entity.Producto;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public interface ProductoRepository extends CrudRepository<Producto,Integer> {
     @Query(value = "SELECT * FROM productos WHERE id_categoria= ? ", nativeQuery = true) //query nativa
     List<Producto>findByIdCategoria(int idCategoria); //query method
