4caeae14f005ecade5d40b43d79c8b0c5978b2d7
Pablo Vallejos <faktory.fm@live.com>
2023-12-19 10:55:45 -0300

    auditoria de usuarios en la base de datos

--- app-market/src/main/java/com/pablovass/persistence/audit/AuditableEntity.java
+++ app-market/src/main/java/com/pablovass/persistence/audit/AuditableEntity.java
@@ -2,7 +2,9 @@ package com.pablovass.persistence.audit;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
@@ -16,4 +18,10 @@ public class AuditableEntity {
     @Column(name = "modified_date")
     @LastModifiedDate
     private LocalDateTime modifiedDate;
+    @CreatedBy
+    @Column(name ="created_by" )
+    private String createdBy;
+    @LastModifiedBy
+    @Column(name ="modified_by" )
+    private String modifiedBy;
 }
